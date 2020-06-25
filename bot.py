a57cae7c9e1df42f8149e3b8bfb16484a4b4c325
VandenMiner <59515058+VandenMiner@users.noreply.github.com>
2020-06-25 13:20:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,61 +6,61 @@ ds = commands.Bot(command_prefix='!')
 import discord
 import asyncio
 import random
-
+colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark_gold(),discord.Color.gold(),discord.Color.dark_magenta(),discord.Color.magenta(),discord.Color.red(),discord.Color.dark_red(),discord.Color.blue(),discord.Color.dark_blue(),discord.Color.teal(),discord.Color.dark_teal(),discord.Color.green(),discord.Color.dark_green(),discord.Color.purple(),discord.Color.dark_purple()]
 @ds.event
 async def on_ready():
-	print(ds.get_all_members)
-	print("Why are you gay?")
-	await ds.change_presence(status=discord.Status.online, activity=discord.Game("Minecraft"))
-	ds.loop.create_task(rainbowrole("Staff"))
+    print(ds.get_all_members)
+    print("Why are you gay?")
+    await ds.change_presence(status=discord.Status.online, activity=discord.Game("Minecraft"))
+    ds.loop.create_task(rainbowrole("Staff"))
 @ds.command(pass_context = True)
 @commands.has_permissions(administrator = True)
 async def clean(ctx, amount = 100):
-	await ctx.channel.purge(limit = amount)
+    await ctx.channel.purge(limit = amount)
 
 
 
 @ds.command(pass_context=True)
 @commands.has_permissions(administrator = True)
 async def accept(ctx, user: discord.Member):
-	channel = ds.get_channel(713364415180832768)
-	emb = discord.Embed(colour = discord.Color.gold())
-	emb.set_footer(icon_url = 'https://media.discordapp.net/attachments/647015015211335700/713387996895903865/Ava_gruppy_vk_1.png?width=677&height=677')
+    channel = ds.get_channel(713364415180832768)
+    emb = discord.Embed(colour = discord.Color.gold())
+    emb.set_footer(icon_url = 'https://media.discordapp.net/attachments/647015015211335700/713387996895903865/Ava_gruppy_vk_1.png?width=677&height=677')
 
-	emb.add_field( name = "_Diadem_", value = "**Привет! Добро пожаловать в семью, "+ str(user.mention) +" :family: \nЖелаем тебе приятной игры!**")
-	await channel.send(embed = emb)
-	await user.send('''**Приветствую тебя, '''+user.mention+''' !
+    emb.add_field( name = "_Diadem_", value = "**Привет! Добро пожаловать в семью, "+ str(user.mention) +" :family: \nЖелаем тебе приятной игры!**")
+    await channel.send(embed = emb)
+    await user.send('''**Приветствую тебя, '''+user.mention+''' !
 Поздравляю тебя с успешным получением роли "Игрок" в discord-сервере проекта Diadem! :tada:**''')
-	role = get(ctx.guild.roles, name='Игрок')
-	await user.add_roles(role)
-	role1 = get(ctx.guild.roles, name='Без роли')
-	await user.remove_roles(role1)
+    role = get(ctx.guild.roles, name='Игрок')
+    await user.add_roles(role)
+    role1 = get(ctx.guild.roles, name='Без роли')
+    await user.remove_roles(role1)
 
 
 
 @ds.event
 async def on_message(message):
-	await ds.process_commands(message)
-	if message.content.startswith('1'):
-		sendchannel = message.channel
-		channelforaccept = ds.get_channel(713364415180832768)
-		print(str(sendchannel) + str(channelforaccept.name))
-		if sendchannel == channelforaccept:
-			channel = ds.get_channel(713368052699365456)
-			author = message.author
-			role = get(message.guild.roles, name = 'Игрок')
-			await channel.send(author.mention + '** подал заявку на получение роли **' + role.mention)
-			vanden = ds.get_user(451826240186351616)
-			await vanden.send(author.mention + '** подал заявку на получение роли **' + role.mention)
-			await vanden.send('**Текст:** `' + str(message.content) + '`')
-		fsendchannel = message.channel
-		fchannelforaccept = ds.get_channel(715184789858287636)
-		print(str(fsendchannel) + str(fchannelforaccept.name))
-		if fsendchannel == fchannelforaccept:
-			author = message.author
-			fchannel = ds.get_channel(713368052699365456)
-			fauthor = message.author
-			await fchannel.send(author.mention + '** подал жалобу **')
+    await ds.process_commands(message)
+    if message.content.startswith('1'):
+        sendchannel = message.channel
+        channelforaccept = ds.get_channel(713364415180832768)
+        print(str(sendchannel) + str(channelforaccept.name))
+        if sendchannel == channelforaccept:
+            channel = ds.get_channel(713368052699365456)
+            author = message.author
+            role = get(message.guild.roles, name = 'Игрок')
+            await channel.send(author.mention + '** подал заявку на получение роли **' + role.mention)
+            vanden = ds.get_user(451826240186351616)
+            await vanden.send(author.mention + '** подал заявку на получение роли **' + role.mention)
+            await vanden.send('**Текст:** `' + str(message.content) + '`')
+        fsendchannel = message.channel
+        fchannelforaccept = ds.get_channel(715184789858287636)
+        print(str(fsendchannel) + str(fchannelforaccept.name))
+        if fsendchannel == fchannelforaccept:
+            author = message.author
+            fchannel = ds.get_channel(713368052699365456)
+            fauthor = message.author
+            await fchannel.send(author.mention + '** подал жалобу **')
 @ds.event
 async def on_member_join(member):
     emojik1 = get(member.guild.emojis, name = "vk")
@@ -100,6 +100,9 @@ async def on_voice_state_update(member,before,after):
                 await channel2.delete()
 
 
+
+
+
 async def rainbowrole(role):
     for role in ds.get_guild(701453861679792195).roles:
         if str(role) == str("Staff"):
@@ -128,35 +131,35 @@ async def rainbowrole(role):
 @ds.command(pass_context = True)
 @commands.has_permissions(administrator = True)
 async def answer(ctx, arg, member: discord.Member):
-	#Жалоба рассмотрена. Нарушитель понёс наказание. n
-	#Благодарим Вас за ваше обращение!:thumbsup_tone1:
-	channel = ds.get_channel(715184789858287636)
-	await channel.send(member.mention)
-	if arg == "1":
-		emb = discord.Embed(colour = discord.Color.green())
-		emb.set_author(name = "Жалоба рассмотрена. Нарушитель понёс наказание.", icon_url= member.avatar_url)
-		emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
-		emb.add_field(name = "\u200b", value = """*⁣Благодарим Вас за ваше обращение!*⁣:thumbsup_tone1:""")
-		await channel.send(embed = emb)
-	elif arg == "2":
-		emb = discord.Embed(colour = discord.Color.green())
-		emb.set_author(name = "Жалоба рассмотрена. Всё ваше имущество восстановлено,  нарушитель понёс наказание.", icon_url= member.avatar_url)
-		emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
-		emb.add_field(name = "\u200b", value = """*Благодарим Вас за ваше обращение!*:thumbsup_tone1:""")
-		await channel.send(embed = emb)
-	elif arg == "3":
-		emb = discord.Embed(colour = discord.Color.green())
-		emb.set_author(name = "⁣Жалоба рассмотрена. Игрок, на которого была подана вами жалоба, получил сообщение от члена админ.состава с целью вернуть вещи/предоставить дополнительную информацию. ⁣", icon_url= member.avatar_url)
-		emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
-		emb.add_field(name = "\u200b", value = """*Благодарим Вас за ваше обращение!*⁣:thumbsup_tone1:""")
-		await channel.send(embed = emb)
-	elif arg == "4":
-		emb = discord.Embed(colour = discord.Color.red())
-		emb.set_author(name = "Отказ в рассмотрении жалобы.\nВозможные причины: «жалоба не по форме»,«нехватка/фальсификация улик».", icon_url= member.avatar_url)
-		emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
-		emb.add_field(name = "\u200b", value = """*Рекомендуем вам пересмотреть свою жалобу.*""")
-		await channel.send(embed = emb)
+    #Жалоба рассмотрена. Нарушитель понёс наказание. n
+    #Благодарим Вас за ваше обращение!:thumbsup_tone1:
+    channel = ds.get_channel(715184789858287636)
+    await channel.send(member.mention)
+    if arg == "1":
+        emb = discord.Embed(colour = discord.Color.green())
+        emb.set_author(name = "Жалоба рассмотрена. Нарушитель понёс наказание.", icon_url= member.avatar_url)
+        emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
+        emb.add_field(name = "\u200b", value = """*⁣Благодарим Вас за ваше обращение!*⁣:thumbsup_tone1:""")
+        await channel.send(embed = emb)
+    elif arg == "2":
+        emb = discord.Embed(colour = discord.Color.green())
+        emb.set_author(name = "Жалоба рассмотрена. Всё ваше имущество восстановлено,  нарушитель понёс наказание.", icon_url= member.avatar_url)
+        emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
+        emb.add_field(name = "\u200b", value = """*Благодарим Вас за ваше обращение!*:thumbsup_tone1:""")
+        await channel.send(embed = emb)
+    elif arg == "3":
+        emb = discord.Embed(colour = discord.Color.green())
+        emb.set_author(name = "⁣Жалоба рассмотрена. Игрок, на которого была подана вами жалоба, получил сообщение от члена админ.состава с целью вернуть вещи/предоставить дополнительную информацию. ⁣", icon_url= member.avatar_url)
+        emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
+        emb.add_field(name = "\u200b", value = """*Благодарим Вас за ваше обращение!*⁣:thumbsup_tone1:""")
+        await channel.send(embed = emb)
+    elif arg == "4":
+        emb = discord.Embed(colour = discord.Color.red())
+        emb.set_author(name = "Отказ в рассмотрении жалобы.\nВозможные причины: «жалоба не по форме»,«нехватка/фальсификация улик».", icon_url= member.avatar_url)
+        emb.set_thumbnail(url = "https://media.discordapp.net/attachments/713367810985689110/714404218777239614/anim.gif")
+        emb.add_field(name = "\u200b", value = """*Рекомендуем вам пересмотреть свою жалобу.*""")
+        await channel.send(embed = emb)
 
 token = os.environ.get("BOT_TOKEN")
 
-ds.run(token)
+ds.run("token")
